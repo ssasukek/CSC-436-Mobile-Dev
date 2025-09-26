015c1a144f06acd3fc35204238763b325975f8a1
Kevin Lu <kevinlu3234@gmail.com>
2025-09-25 18:20:23 -0700

    Add GraduationRSVP project

--- GraduationRSVP/app/src/main/java/com/zybooks/graduationrsvp/MainActivity.kt
+++ GraduationRSVP/app/src/main/java/com/zybooks/graduationrsvp/MainActivity.kt
@@ -4,7 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Info
@@ -14,6 +20,11 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
@@ -47,7 +58,7 @@ fun GradScreen(modifier: Modifier = Modifier) {
         painter = painterResource(R.drawable.grad_cap),
         contentDescription = "Graduation cap",
         alpha = 0.3f,
-        modifier = modifier.size(200.dp)
+        modifier = modifier
     )
     Text(
         text = stringResource(R.string.graduation_announcement),
@@ -57,28 +68,48 @@ fun GradScreen(modifier: Modifier = Modifier) {
         color = Color.Red,
         textAlign = TextAlign.Center,
         modifier = modifier
+            .offset(y = 70.dp)
     )
-    Icon(
-        imageVector = Icons.Filled.Info,
-        contentDescription = "Event Icon",
-        tint = Color.Gray,
-        modifier = modifier.size(100.dp)
+    var checked by remember { mutableStateOf(false) }
+    Row(
+        modifier = Modifier
+            .offset(x = 20.dp ,y = 650.dp),
+    ){
+        Row(verticalAlignment = Alignment.CenterVertically){
+            Icon(
+                imageVector = Icons.Filled.Info,
+                contentDescription = "Event Icon",
+                tint = Color.Gray,
+                modifier = modifier
+                    .size(50.dp)
+            )
+            Text(
+                text = "May 14 - Ganus Hall - 2:00 pm",
+                fontSize = 22.sp,
+                color = Color.Black,
+                textAlign = TextAlign.Center,
+                modifier = modifier
+            )
+        }
 
-    )
-    Text(
-        text = "May 14 - Ganus Hall - 2:00 pm",
-        fontSize = 15.sp,
-        color = Color.Gray,
-        textAlign = TextAlign.Center,
-        modifier = modifier
-    )
-//    Switch(
-//        checked =
-//    )
-//    Text(
-//        text = if (checked) "I'll be there!" else "Sorry I can't be there.",
-//        fontSize = 20.sp
-//    )
+    }
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .offset(y = 750.dp)
+    ){
+        Switch(
+            checked = checked,
+            onCheckedChange = { checked = it },
+            modifier = modifier
+        )
+        Text(
+            text = if (checked) "I'll be there!" else "Sorry I can't be there.",
+            fontSize = 35.sp,
+            textAlign = TextAlign.Center,
+            modifier = modifier
+        )
+    }
 }
 
 @Preview(showBackground = true)
